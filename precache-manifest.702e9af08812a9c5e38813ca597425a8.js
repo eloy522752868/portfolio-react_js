8e8b964bcff836f4a0874e9b69a844896cd6cb15
Eloy Gonzalez <eloy522752@gmail.com>
2021-05-19 12:48:48 -0400

    Updates

---
+++ precache-manifest.702e9af08812a9c5e38813ca597425a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13985a63acde5a929ada",
+    "url": "/portfolio-react/static/css/main.3cf54449.chunk.css"
+  },
+  {
+    "revision": "13985a63acde5a929ada",
+    "url": "/portfolio-react/static/js/main.13985a63.chunk.js"
+  },
+  {
+    "revision": "780bcbc715c88212df47",
+    "url": "/portfolio-react/static/js/runtime~main.780bcbc7.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio-react/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "7306ac8351b159032d08a1541de72b61",
+    "url": "/portfolio-react/index.html"
+  }
+];
